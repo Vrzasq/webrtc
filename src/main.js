d2b72abf791d9d8b2010d09949e8e6d5f8d8fb22
Jakub Romel <ridzio1986@hotmail.com>
2018-06-18 21:59:48 +0200

    add fron camera support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 "use strict"
 
-const constraints = {
+const frontCameraConstraints = {
+    audio: true,
+    video: {
+        width: { min: 640, max: 1280 },
+        height: { min: 480, max: 720 },
+        facingMode: { exact: "user" }
+    }
+};
+
+const backCameraConstraints = {
     audio: true,
     video: {
         width: { min: 640, max: 1280 },
@@ -9,16 +18,24 @@ const constraints = {
     }
 };
 
+
 // Video element where stream will be placed.
-const localVideo = document.querySelector('video');
+const frontCamera = document.getElementById('front-camera');
+const backCamera = document.getElementById('back-camera');
 
 // Local stream that will be reproduced on the video.
-let localStream;
+let frontCameraStream;
+let backCameraStream;
 
 // Handles success by adding the MediaStream to the video element.
-function gotLocalMediaStream(mediaStream) {
-    localStream = mediaStream;
-    localVideo.srcObject = mediaStream;
+function gotLocalFrontMediaStream(mediaStream) {
+    frontCameraStream = mediaStream;
+    frontCamera.srcObject = mediaStream;
+}
+
+function gotLocalBackMediaStream(mediaStream) {
+    frontCameraStream = mediaStream;
+    frontCamera.srcObject = mediaStream;
 }
 
 // Handles error by logging a message to the console with the error message.
@@ -27,5 +44,8 @@ function handleLocalMediaStreamError(error) {
 }
 
 // Initializes media stream.
-navigator.mediaDevices.getUserMedia(constraints)
-    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+navigator.mediaDevices.getUserMedia(frontCameraConstraints)
+    .then(gotLocalFrontMediaStream).catch(handleLocalMediaStreamError);
+
+navigator.mediaDevices.getUserMedia(backCameraConstraints)
+    .then(gotLocalBackMediaStream).catch(handleLocalMediaStreamError);
